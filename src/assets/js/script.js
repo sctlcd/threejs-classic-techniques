62fedc26ddacb316153697c7339647d18199e351
sctlcd <lucinda.souchet@gmail.com>
2023-11-16 10:23:52 +0100

    updated scripts.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,17 +1,17 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff_00_00 })
-const mesh = new THREE.Mesh(geometry, material)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff_00_00 });
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
 /**
@@ -20,20 +20,20 @@ scene.add(mesh)
 const sizes = {
     width: 800,
     height: 600
-}
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
